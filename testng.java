6db5410045487d6f3a3b9d2b43dd529ff0d80379
Iamneo <techadmin@iamneo.ai>
2023-08-13 09:14:46 +0000

    Backup Commit

--- testng.java
+++ testng.java
@@ -2,44 +2,65 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class LinkTest {
+import java.util.List;
 
+public class FlipkartLinkTest {
     private WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        // Set the path to the Chrome driver executable
-        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver.exe");
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
 
-        // Initialize the Chrome driver
+        // Create a new instance of the ChromeDriver
         driver = new ChromeDriver();
+
+        // Maximize the browser window
         driver.manage().window().maximize();
     }
 
     @Test
-    public void testCountAndPrintLinks() {
-        // Navigate to "https://flipkart.com"
+    public void testCountLinksOnHomePage() {
+        // Open the Flipkart website
+        driver.get("https://flipkart.com");
+
+        // Find all the links on the homepage
+        List<WebElement> links = driver.findElements(By.tagName("a"));
+
+        // Get the count of links
+        int linkCount = links.size();
+
+        // Assert the link count is greater than 0
+        Assert.assertTrue(linkCount > 0, "No links found on the homepage.");
+        System.out.println("Number of links on the homepage: " + linkCount);
+    }
+
+    @Test
+    public void testPrintLinkTextAndURLs() {
+        // Open the Flipkart website
         driver.get("https://flipkart.com");
 
         // Find all the links on the page
-        java.util.List<WebElement> links = driver.findElements(By.tagName("a"));
-
-        // Print the number of links on the page
-        System.out.println("Number of links on the page: " + links.size());
+        List<WebElement> links = driver.findElements(By.tagName("a"));
 
         // Print link text and URLs
         for (WebElement link : links) {
-            System.out.println("Link Text: " + link.getText());
-            System.out.println("Link URL: " + link.getAttribute("href"));
-            System.out.println("-------------------------");
+            String linkText = link.getText();
+            String linkURL = link.getAttribute("href");
+            System.out.println("Link Text: " + linkText);
+            System.out.println("Link URL: " + linkURL);
+            System.out.println("----------------------------");
         }
     }
 
     @AfterMethod
     public void tearDown() {
-        // Close the browser
+        // Quit the browser
         driver.quit();
-    }
+    }
 }
